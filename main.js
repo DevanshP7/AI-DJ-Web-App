793bf4f1889af5a4ebee3aeb2fbd5d9707092edf
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2022-04-19 18:32:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song = '';
+
+function setup(){
+
+    canvas = createCanvas(640,480);
+    canvas.position(630, 350);
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+
+    image(video, 0, 0 ,640, 480);
+}
+
+function preload(){
+    song = loadSound('music.mp3');
+}   
+
+function play_music(){
+    song.play();
+}
